8197de8817c8ecc056ff85546576cbc8e36911c2
zerinhomilgrau <gab.schlichting22@gmail.com>
2023-06-30 09:43:06 -0300

    Login added

---
+++ src/screens/Login/Login.jsx
@@ -0,0 +1,159 @@
+import React from "react";
+import "./style.css";
+
+export const Login = () => {
+  return (  
+    <div className="login">
+        <div className="div">
+            <div className="native-keyboard">
+                <div className="overlap">
+                    <div className="background">
+                        <img className="native-home" alt="Native home" src="https://generation-sessions.s3.amazonaws.com/8fda09dd27c1051e17d1c3c6f6e140d7/img/native---home-indicator.svg" />
+                    </div>
+                    <div className="keys">
+                        <img className="dictation" alt="Dictation"
+                        src="https://generation-sessions.s3.amazonaws.com/8fda09dd27c1051e17d1c3c6f6e140d7/img/dictation.svg" />
+                        <img className="emoji" alt="Emoji"
+                        src="https://generation-sessions.s3.amazonaws.com/8fda09dd27c1051e17d1c3c6f6e140d7/img/emoji.svg" />
+                        <div className="return">
+                            <div className="label">Go</div>
+                        </div>
+                        <div className="space">
+                            <div className="text-wrapper">Space</div>
+                        </div>
+                        <div className="element">
+                            <div className="label-2">123</div>
+                        </div>
+                        <div className="delete">
+                            <div className="overlap-group">
+                                <div className="key-light">
+                                    <div className="label-3" />
+                                </div>
+                                <img className="delete-button" alt="Delete button" src="https://generation-sessions.s3.amazonaws.com/8fda09dd27c1051e17d1c3c6f6e140d7/img/delete-button.svg" />
+                            </div>
+                        </div>
+                        <div className="shift">
+                            <div className="overlap-2">
+                                <div className="symbol-wrapper">
+                                    <div className="symbol" />
+                                </div>
+                                <img className="img" alt="Shift" src="https://generation-sessions.s3.amazonaws.com/8fda09dd27c1051e17d1c3c6f6e140d7/img/shift.svg" />
+                            </div>
+                        </div>
+                        <div className="m">
+                            <div className="symbol-2">M</div>
+                        </div>
+                        <div className="n">
+                            <div className="symbol-3">N</div>
+                        </div>
+                        <div className="b">
+                            <div className="symbol-4">B</div>
+                        </div>
+                        <div className="v">
+                            <div className="symbol-3">V</div>
+                        </div>
+                        <div className="c">
+                            <div className="symbol-3">C</div>
+                        </div>
+                        <div className="x">
+                            <div className="symbol-4">X</div>
+                        </div>
+                        <div className="z">
+                            <div className="symbol-4">Z</div>
+                        </div>
+                        <div className="l">
+                            <div className="symbol-5">L</div>
+                        </div>
+                        <div className="k">
+                            <div className="symbol-4">K</div>
+                        </div>
+                        <div className="j">
+                            <div className="symbol-5">J</div>
+                        </div>
+                        <div className="h">
+                            <div className="symbol-3">H</div>
+                        </div>
+                        <div className="g">
+                            <div className="symbol-3">G</div>
+                        </div>
+                        <div className="f">
+                            <div className="symbol-4">F</div>
+                        </div>
+                        <div className="d">
+                            <div className="symbol-3">D</div>
+                        </div>
+                        <div className="s">
+                            <div className="symbol-4">S</div>
+                        </div>
+                        <div className="a">
+                            <div className="symbol-3">A</div>
+                        </div>
+                        <div className="p">
+                            <div className="symbol-4">P</div>
+                        </div>
+                        <div className="o">
+                            <div className="symbol-6">O</div>
+                        </div>
+                        <div className="i">
+                            <div className="symbol-7">I</div>
+                        </div>
+                        <div className="u">
+                            <div className="symbol-3">U</div>
+                        </div>
+                        <div className="y">
+                            <div className="symbol-4">Y</div>
+                        </div>
+                        <div className="t">
+                            <div className="symbol-4">T</div>
+                        </div>
+                        <div className="r">
+                            <div className="symbol-4">R</div>
+                        </div>
+                        <div className="e">
+                            <div className="symbol-4">E</div>
+                        </div>
+                        <div className="w">
+                            <div className="symbol-8">W</div>
+                        </div>
+                        <div className="q">
+                            <div className="symbol-6">Q</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="log-in-button">
+                <div className="text">Log in</div>
+            </div>
+            <p className="by-continuing-you">
+                <span className="span">By continuing, you agree to our</span>
+                <span className="text-wrapper-2">Terms of Service</span>
+                <span className="span">and</span>
+                <span className="text-wrapper-2">Privacy Policy</span>
+            </p>
+            <div className="email-password">
+                <div className="text-wrapper-3">Esqueceu sua senha?</div>
+                <div className="password">
+                    <div className="partials-text-field">
+                        <input className="input" />
+                    </div>
+                </div>
+                <div className="email">
+                    <div className="partials-text-field-2">
+                        <div className="text-wrapper-4">Email</div>
+                        <input className="text-2" />
+                    </div>
+                </div>
+            </div>
+            <div className="div-wrapper">
+                <div className="text-wrapper-5">Log in</div>
+            </div>
+            <div className="native-status-bar">
+                <div className="text-wrapper-6">9:41</div>
+                <div className="mobile-signal" />
+                <div className="wif" />
+                <img className="battery" alt="Battery" src="https://generation-sessions.s3.amazonaws.com/8fda09dd27c1051e17d1c3c6f6e140d7/img/battery@2x.png" />
+            </div>
+        </div>
+    </div>
+    );
+};
